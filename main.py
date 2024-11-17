baae3ab925202debf4be3ee71321ca9fd294aaa4
UltraQbik <no1skill@yandex.ru>
2024-11-17 04:19:39 +0300

    Some kind of module loading

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import os
 import sys
 import json
 import discord
+import logging
 from discord.ext import commands
+
+
+MODULES_DIRECTORY = "modules"
+CONFIGS_DIRECTORY = "configs"
 
 
 class Client(commands.Bot):
@@ -13,6 +18,51 @@ class Client(commands.Bot):
     def __init__(self):
         super(Client, self).__init__(command_prefix="!", intents=discord.Intents.all(),
                                      help_command=None)
+
+        self.logger: logging.Logger = logging.getLogger(__name__)
+
+        self.modules_present: list[str] = list()
+        self.modules_queued: list[str] = list()
+        self.modules_running: list[str] = list()
+
+        self._load_modules()
+
+    def _load_modules(self):
+        """
+        Loads 'self.modules_present' and 'self.modules_running' lists
+        """
+
+        for present_module in os.listdir(MODULES_DIRECTORY):
+            # check for complete module
+            if os.path.isfile(f"{MODULES_DIRECTORY}/{present_module}/main.py"):
+                self.modules_present.append(present_module)
+            else:
+                self.logger.warning(f"Malformed module '{present_module}'; missing 'main.py'")
+
+        if not os.path.isfile(f"{CONFIGS_DIRECTORY}/modules.json"):
+            raise FileNotFoundError("Missing 'modules.json'")
+        with open(f"{CONFIGS_DIRECTORY}/modules.json", "r", encoding="ascii") as file:
+            modules: list[str] = json.loads(file.read())
+        for queued_module in modules:
+            # if queued module does not exist in modules directory
+            if queued_module not in self.modules_present:
+                self.logger.warning(f"Module '{queued_module}' not found")
+                continue
+            self.modules_queued.append(queued_module)
+
+    async def setup_hook(self) -> None:
+        """
+        Module loading after the client's successful connection
+        """
+
+        for queued_module in self.modules_queued:
+            try:
+                await self.load_extension(f"{MODULES_DIRECTORY}.{queued_module}.main")
+            except commands.ExtensionError as e:
+                self.logger.warning(f"Module '{queued_module}' failure", exc_info=e)
+                continue
+            self.modules_running.append(queued_module)
+            self.modules_queued.remove(queued_module)
 
 
 def main():
