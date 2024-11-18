b2ab5e9a9ea8f6bffa0b8644ac5d8836c8cf4528
UltraQbik <no1skill@yandex.ru>
2024-11-18 22:23:09 +0300

    Add method 'fetch_channel_info' Started working on async too

--- modules/YouTubeNotifs/fetcher.py
+++ modules/YouTubeNotifs/fetcher.py
@@ -2,7 +2,9 @@
 YouTube's videos and streams fetching
 """
 
-import requests
+import json
+import asyncio
+import aiohttp
 from source.settings import YOUTUBE_API_KEY
 
 
@@ -15,17 +17,83 @@ class Fetcher:
     # "channel_id": "upload_id"
     channel_upload_playlists: dict[str, str] = {}
 
+    @staticmethod
+    async def fetch_channel_info(channel_id: str):
+        """
+        Fetches information about a given channel.
+        :param channel_id: channel id
+        :return: channel information dictionary. Direct API response
+        """
+
+        """
+        Example response:
+        {
+          "kind": "youtube#channelListResponse",
+          "etag": "IS9PazQv3-yvhjZ-XZTgoT-ez40",
+          "pageInfo": {
+            "totalResults": 1,
+            "resultsPerPage": 5
+          },
+          "items": [
+            {
+              "kind": "youtube#channel",
+              "etag": "1xnwFMBecF998HoKDSJ3rtCOB9o",
+              "id": "UCL-8FVaefmqox59LpOJxnOQ",
+              "snippet": {
+                "title": "Qubik",
+                "description": "...",
+                "customUrl": "@qubane",
+                "publishedAt": "2017-05-06T16:08:53Z",
+                "thumbnails": {
+                  "default": {
+                    "url": "...",
+                    "width": 88,
+                    "height": 88
+                  },
+                  "medium": {
+                    "url": "...",
+                    "width": 240,
+                    "height": 240
+                  },
+                  "high": {
+                    "url": "...",
+                    "width": 800,
+                    "height": 800
+                  }
+                },
+                "localized": {
+                  "title": "Qubik",
+                  "description": "..."
+                },
+                "country": "RU"
+              }
+            }
+          ]
+        }
+        """
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                    f"https://www.googleapis.com/youtube/v3/channels?"
+                    f"part=snippet&"
+                    f"id={channel_id}&"
+                    f"key={YOUTUBE_API_KEY}",
+                    headers={"Accept-Encoding": "gzip,deflate"}) as resp:
+                response = await resp.json()
+
+        return response["items"][0]["snippet"]
+
     @classmethod
-    def fetch_videos(cls, channel_id: str, amount: int) -> dict[str, str | dict]:
+    async def fetch_videos(cls, channel_id: str, amount: int) -> dict[str, str | dict]:
         """
         Returns a list of videos on the channel.
         :param channel_id: channel id
         :param amount: amount of videos to fetch
-        :return: list of video ids
+        :return: list of video ids. items part of API response
         """
 
         """
-        Example return:
+        Example response:
         {
           "kind": "youtube#channelListResponse",
           "etag": "-MN_iD2qDxM2j7txDFiyj2v_9mo",
@@ -63,7 +131,7 @@ class Fetcher:
             uploads_id = cls.channel_upload_playlists[channel_id]
 
         # fetch last {amount} videos
-        playlist = requests.get(
+        playlist = await requests.get(
             f"https://www.googleapis.com/youtube/v3/playlistItems?"
             f"part=snippet%2CcontentDetails&"
             f"maxResults={amount}&"
@@ -74,9 +142,10 @@ class Fetcher:
         return playlist["items"]
 
 
-def test():
-    print(Fetcher.fetch_videos(r"UCL-8FVaefmqox59LpOJxnOQ", 10))
+async def test():
+    # print(Fetcher.fetch_videos(r"UCL-8FVaefmqox59LpOJxnOQ", 10))
+    print(json.dumps(await Fetcher.fetch_channel_info(r"UCL-8FVaefmqox59LpOJxnOQ"), indent=2))
 
 
 if __name__ == '__main__':
-    test()
+    asyncio.run(test())
