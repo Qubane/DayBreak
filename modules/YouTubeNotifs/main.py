4d0a227b65ba5bc746626c1e1ff5924979198393
UltraQbik <no1skill@yandex.ru>
2024-12-18 20:45:18 +0300

    Functional YTNotifications

--- modules/YouTubeNotifs/main.py
+++ modules/YouTubeNotifs/main.py
@@ -10,7 +10,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands, tasks
 from source.settings import CONFIGS_DIRECTORY
-from modules.YouTubeNotifs.fetcher import Fetcher, Video
+from modules.YouTubeNotifs.fetcher import Fetcher, Video, Channel
 
 
 class YouTubeNotifsModule(commands.Cog):
@@ -27,7 +27,10 @@ class YouTubeNotifsModule(commands.Cog):
 
         # youtube channels
         # {"channel_id": [Video(...), Video(...), ...]}
-        self.channels: dict[str, list[Video]] = dict()
+        self.channels_videos: dict[str, list[Video]] = dict()
+
+        # "channel_id": Channel(...)
+        self.channels: dict[str, Channel] = dict()
         self.channels_init: bool = False
 
         self.load_configs()
@@ -68,7 +71,7 @@ class YouTubeNotifsModule(commands.Cog):
         # return channel dict
         return channel_dict
 
-    @tasks.loop(minutes=10)
+    @tasks.loop(minutes=1)
     async def check(self) -> None:
         """
         Checks every 10 minutes for a new video/stream
@@ -77,7 +80,9 @@ class YouTubeNotifsModule(commands.Cog):
         # if channels list is not init, initialize it
         if not self.channels_init:
             self.channels_init = True
-            self.channels = await self.retrieve_channel_videos()
+            self.channels_videos = await self.retrieve_channel_videos()
+            self.channels = {
+                channel_id: await Fetcher.fetch_channel_info(channel_id) for channel_id in self.channels_videos.keys()}
 
             # no need to continue, because we just fetched 'the newest' videos
             return
@@ -90,7 +95,7 @@ class YouTubeNotifsModule(commands.Cog):
             notification_channel = self.client.get_channel(guild_config["notifications_channel_id"])
             notification_format = guild_config["format"]
             video_role_ping = f"<@&{guild_config['video_role_id']}>"
-            stream_role_ping = f"<@&{guild_config['stream_role_id']}>"
+            # stream_role_ping = f"<@&{guild_config['stream_role_id']}>"  # unused
 
             # check every YT channel
             for channel_id in guild_config["youtube_channels"]:
@@ -98,12 +103,22 @@ class YouTubeNotifsModule(commands.Cog):
                 # don't check last new video to prevent old videos to be considered new (ex. deleted video)
                 for new_video in new_channels[channel_id][:-1]:
                     # if new video is not in old videos, then make an announcement
-                    if new_video.id not in self.channels[channel_id]:
-                        # TODO: make announcement
-                        break
+                    if new_video not in self.channels_videos[channel_id]:
+                        msg = notification_format.format(
+                            role_mention=video_role_ping,
+                            channel_name=self.channels[channel_id].title,
+                            channel_url=f"https://www.youtube.com/{self.channels[channel_id].custom_url}",
+                            channel_thumbnail_url=self.channels[channel_id].thumbnails["default"].url,
+                            channel_country=self.channels[channel_id].country,
+                            video_url=f"https://youtu.be/{new_video.id}",
+                            video_title=new_video.title,
+                            video_description=new_video.description,
+                            video_thumbnail_url=new_video.thumbnails["default"].url,
+                            video_publish_date=new_video.published_at)
+                        await notification_channel.send(msg)
 
         # reassign new_channels to self.channels
-        self.channels = new_channels
+        self.channels_videos = new_channels
 
 
 async def setup(client: commands.Bot) -> None:
