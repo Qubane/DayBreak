376053616eb35f46235f1a9b7fa958d8273f63e0
UltraQbik <no1skill@yandex.ru>
2024-12-23 01:29:42 +0300

    Bring back better timeouts from MigthyOmegaBot

--- modules/Utils/main.py
+++ modules/Utils/main.py
@@ -5,6 +5,8 @@ Commands such as: latency; reload_cog; etc
 
 
 import discord
+import logging
+from datetime import timedelta
 from discord import app_commands
 from discord.ext import commands
 
@@ -16,6 +18,8 @@ class UtilsModule(commands.Cog):
 
     def __init__(self, client: commands.Bot) -> None:
         self.client = client
+
+        self.logger: logging.Logger = logging.getLogger(__name__)
 
     @app_commands.command(name="latency", description="shows bots latency")
     async def latency(
@@ -32,6 +36,80 @@ class UtilsModule(commands.Cog):
             color=discord.Color.brand_green())
 
         await interaction.response.send_message(embed=embed, ephemeral=True)
+
+    @app_commands.command(name="btimeout", description="timeouts a user")
+    @app_commands.checks.has_permissions(moderate_members=True)
+    @app_commands.describe(
+        user="User to timeout",
+        seconds="how many seconds to timeout for",
+        minutes="how many minutes to timeout for",
+        hours="how many hours to timeout for",
+        days="how many days to timeout for",
+        weeks="how many weeks to timeout for",
+        reason="reason for a timeout (default is 'bad behaviour')")
+    async def better_timeout(
+            self,
+            interaction: discord.Interaction,
+            user: discord.Member,
+            seconds: int = 0,
+            minutes: int = 0,
+            hours: int = 0,
+            days: int = 0,
+            weeks: int = 0,
+            reason: str = "bad behaviour"
+    ) -> None:
+        """
+        Command that will time you out.
+        Code taken from 'UltraQbik/MightyOmegaBot'
+        """
+
+        # calculate duration, and give a timeout
+        duration = timedelta(seconds=seconds, minutes=minutes, hours=hours, days=days, weeks=weeks)
+
+        # if total time is ~0, make 60 seconds
+        if duration.total_seconds() < 0.1:
+            duration = timedelta(seconds=60)
+
+        # give timeout
+        await user.timeout(duration, reason=reason)
+
+        # make a pretty embed
+        author_embed = discord.Embed(title="Success!", description=f"User {user.name} was put on a timeout",
+                                     color=discord.Color.green())
+
+        # send the pretty embed
+        await interaction.response.send_message(embed=author_embed, ephemeral=True)
+
+        # make a pretty embed
+        user_embed = discord.Embed(title="Timeout!", description="You were put on a timeout",
+                                   color=discord.Color.red())
+        user_embed.add_field(name="Reason", value=reason, inline=True)
+        user_embed.add_field(name="Duration", value=duration.__str__())
+        user_embed.set_author(name=interaction.user.name, icon_url=interaction.user.avatar.url)
+
+        # try to send user a dm with a reason for a timeout
+        try:
+            await user.send(embed=user_embed)
+
+        # if it failed for these reasons, just ignore
+        except (discord.HTTPException, discord.Forbidden):
+            pass
+
+        # if something else failed, print a message
+        except Exception as e:
+            self.logger.warning("An error had occurred while sending timeout message to user", exc_info=e)
+
+    @better_timeout.error
+    async def better_timeout_handler(self, interaction: discord.Interaction, error: discord.DiscordException):
+        """
+        Error handler for better timeout app command
+        """
+
+        if isinstance(error, app_commands.CommandInvokeError):
+            await interaction.response.send_message("User has more permissions than the bot", ephemeral=True)
+        else:
+            self.logger.warning("An error had occurred while handling another error", exc_info=error)
+            await interaction.response.send_message("Unexpected error, check logs for more info", ephemeral=True)
 
 
 async def setup(client: commands.Bot) -> None:
