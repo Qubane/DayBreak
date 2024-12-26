2d97f4a7e07c6a48cc87aa8c984f9a29d05f644b
UltraQbik <no1skill@yandex.ru>
2024-12-26 20:52:05 +0300

    Update settings.py Now creates logs directory

--- source/settings.py
+++ source/settings.py
@@ -16,9 +16,19 @@ YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")  # YouTube Data API key
 MODULES_DIRECTORY = "modules"
 CONFIGS_DIRECTORY = "configs"
 VARS_DIRECTORY = "var"
+LOGS_DIRECTORY = "logs"
 
 
 def init():
+    # create 'var' directory
+    # 'var' directory is used for storing data for modules
+    if not os.path.isdir(VARS_DIRECTORY):
+        os.mkdir(VARS_DIRECTORY)
+
+    # create 'logs' directory
+    if not os.path.isdir(LOGS_DIRECTORY):
+        os.mkdir(LOGS_DIRECTORY)
+
     # setup logging
     logging.basicConfig(
         level=logging.INFO,
@@ -27,13 +37,8 @@ def init():
         format="[{asctime}] [{levelname:<8}] {name}: {message}",
         handlers=[
             logging.handlers.RotatingFileHandler(
-                filename="logs/discord.log",
+                filename=f"{LOGS_DIRECTORY}/discord.log",
                 encoding="utf-8",
                 maxBytes=2**20 * 32,  # 32 MiB
                 backupCount=5),
             logging.StreamHandler()])
-
-    # create 'var' directory
-    # 'var' directory is used for storing data for modules
-    if not os.path.isdir("var"):
-        os.mkdir("var")
