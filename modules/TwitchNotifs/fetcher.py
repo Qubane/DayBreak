080de17be4d65cef4d43c0c4c5a5db98536f2b0a
UltraQbik <no1skill@yandex.ru>
2024-12-30 01:55:00 +0300

    Add new function to get title

--- modules/TwitchNotifs/fetcher.py
+++ modules/TwitchNotifs/fetcher.py
@@ -5,6 +5,7 @@ Twitch stream fetching
 
 import asyncio
 import aiohttp
+from bs4 import BeautifulSoup
 
 
 async def check_live(channel_name: str) -> bool:
@@ -19,9 +20,21 @@ async def check_live(channel_name: str) -> bool:
             return True if 'isLiveBroadcast' in (await resp.text("utf-8")) else False
 
 
+async def get_title(channel_name: str) -> str:
+    """
+    Returns stream's title
+    :param channel_name: name of the twitch channel
+    :return: stream title
+    """
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://www.twitch.tv/{channel_name}") as resp:
+            soup = BeautifulSoup(await resp.read(), features="html.parser")
+    return soup.find("meta", property="og:description")["content"]
+
+
 async def test():
-    print(await check_live("pewdiepie"))
-    print(await check_live("CodeMaker_4"))
+    print(await get_title("mutzbunny"))
 
 
 if __name__ == '__main__':
