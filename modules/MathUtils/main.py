c1c0065b29ef93cef1e0eb627a7f87d6e38879f4
UltraQbik <no1skill@yandex.ru>
2025-01-07 09:16:10 +0300

    Refactor Separate latex rendering into staticmethod

--- modules/MathUtils/main.py
+++ modules/MathUtils/main.py
@@ -11,7 +11,6 @@ from io import BytesIO
 from PIL import Image, ImageOps
 from discord import app_commands
 from discord.ext import commands
-from source.settings import VARS_DIRECTORY
 
 
 class MathUtilsModule(commands.Cog):
@@ -26,32 +25,48 @@ class MathUtilsModule(commands.Cog):
         self.logger: logging.Logger = logging.getLogger(__name__)
         self.logger.info("Module loaded")
 
+    @staticmethod
+    def render_latex_formula(formula: str) -> BytesIO:
+        """
+        Renders formatted LaTeX formula
+        :param formula: LaTeX formula
+        :return: bytesio image
+        """
+
+        img = BytesIO()
+        sympy.preview(
+            f"$${formula}$$",
+            output="png",
+            viewer="BytesIO",
+            outputbuffer=img,
+            euler=False,
+            dvioptions=['-D', '400'])
+        pil_image = ImageOps.expand(Image.open(img), border=20, fill='white')
+        img.seek(0)
+        pil_image.save(img, "PNG")
+        img.seek(0)
+
+        return img
+
     @app_commands.command(name="latex", description="converts LaTeX formula to image")
     @app_commands.describe(text="LaTeX formatted formula")
-    async def render_latex_formula(self, interaction: discord.Interaction, text: str):
+    async def render_latex_formula_cmd(
+            self,
+            interaction: discord.Interaction,
+            text: str
+    ) -> None:
         """
         Command that adds LaTeX rendering
         """
 
-        img = BytesIO()
         try:
-            sympy.preview(
-                f"$${text}$$",
-                output="png",
-                viewer="BytesIO",
-                outputbuffer=img,
-                euler=False,
-                dvioptions=['-D', '400'])
+            img = self.render_latex_formula(text)
         except RuntimeError as exc:
             error = exc.__str__()
             error = error[error.find("! ")+2:]
             error = error[:error.find("\\r")]
             await interaction.response.send_message(error, ephemeral=True)
         else:
-            pil_image = ImageOps.expand(Image.open(img), border=20, fill='white')
-            img.seek(0)
-            pil_image.save(img, "PNG")
-            img.seek(0)
             await interaction.response.send_message(file=discord.File(img, filename="result.png"))
 
 
