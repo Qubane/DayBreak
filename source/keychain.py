bac73c572620de017b1f02a92a999fb61c8bb2ec
UltraQbik <no1skill@yandex.ru>
2025-05-30 17:13:19 +0300

    Add key(cls, name: str) -> str & _update_keychain(cls) methods

--- source/keychain.py
+++ source/keychain.py
@@ -8,3 +8,28 @@ class KeyChain:
     Manages API Keys
     """
 
+    # keychain mapping
+    _mapping: dict[str, str]
+
+    @classmethod
+    def key(cls, name: str) -> str:
+        """
+        Returns API key.
+        :param name: API key name
+        :return: API key string
+        :raises: KeyError when key is not found
+        """
+
+        # if name is missing, try to update the keychain
+        if name not in cls._mapping:
+            cls._update_keychain()
+
+        # return the API key.
+        # raise an error if the key cannot be fetched
+        return cls._mapping[name]
+
+    @classmethod
+    def _update_keychain(cls):
+        """
+        Updates KeyChain class
+        """
