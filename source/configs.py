1ea9cea1aee11349338cb28768646ef7d9e9d1fc
UltraQbik <no1skill@yandex.ru>
2025-07-11 01:51:45 +0300

    Config reading

--- source/configs.py
+++ source/configs.py
@@ -3,6 +3,8 @@ Handling loading of configurations for modules
 """
 
 
+import json
+from collections import namedtuple
 from source.settings import CONFIGS_MODULES_DIRECTORY, CONFIGS_GUILDS_DIRECTORY
 
 
@@ -11,8 +13,26 @@ class ModuleConfig:
     Container for per-module configuration
     """
 
+    def __init__(self, module_name: str):
+        self.config_path: str = f"{CONFIGS_MODULES_DIRECTORY}/{module_name}"
+
+        # load raw config
+        with open(self.config_path, "r", encoding="utf-8") as file:
+            self._config = json.load(file)
+
+        # TODO: attribute conversion
+
 
 class GuildConfig:
     """
     Container for per-guild module configuration
     """
+
+    def __init__(self, guild_id: str | int):
+        self.config_path: str = f"{CONFIGS_GUILDS_DIRECTORY}/{guild_id}"
+
+        # load raw config
+        with open(self.config_path, "r", encoding="utf-8") as file:
+            self._config = json.load(file)
+
+        # TODO: attribute conversion
