ac31c26af3365a66abd6a70437ce22a89f75c8e9
UltraQbik <no1skill@yandex.ru>
2025-07-11 01:18:16 +0300

    Add CONFIGS_MODULES_DIRECTORY and CONFIGS_GUILDS_DIRECTORY

--- source/settings.py
+++ source/settings.py
@@ -8,28 +8,42 @@ import logging
 import logging.handlers
 
 
-# paths
-MODULES_DIRECTORY = "modules"
-CONFIGS_DIRECTORY = "configs"
-VARS_DIRECTORY = "var"
-LOGS_DIRECTORY = "logs"
-KEYS_DIRECTORY = "keys"
+# root paths
+MODULES_DIRECTORY = "modules"   # cogs source code
+CONFIGS_DIRECTORY = "configs"   # bot configurations
+VARS_DIRECTORY = "var"          # modules storage
+LOGS_DIRECTORY = "logs"         # bot logs
+
+# sub paths
+KEYS_DIRECTORY = f"{CONFIGS_DIRECTORY}/keys"                # references to keys used by modules
+CONFIGS_MODULES_DIRECTORY = f"{CONFIGS_DIRECTORY}/modules"  # global module configs
+CONFIGS_GUILDS_DIRECTORY = f"{CONFIGS_DIRECTORY}/guilds"    # per-guild module configs
 
 
 def init():
     # create 'var' directory
     # 'var' directory is used for storing data for modules
     if not os.path.isdir(VARS_DIRECTORY):
-        os.mkdir(VARS_DIRECTORY)
+        os.makedirs(VARS_DIRECTORY)
 
     # create 'keys' directory
     # 'keys' directory is used for references to keynames for the KeyChain class
     if not os.path.isdir(KEYS_DIRECTORY):
-        os.mkdir(KEYS_DIRECTORY)
+        os.makedirs(KEYS_DIRECTORY)
+
+    # create 'modules' directory
+    # 'modules' directory is used by modules for storing their global configuration
+    if not os.path.isdir(CONFIGS_MODULES_DIRECTORY):
+        os.makedirs(CONFIGS_MODULES_DIRECTORY)
+
+    # create 'guilds' directory
+    # 'guilds' directory is used by modules for storing per-guild configuration
+    if not os.path.isdir(CONFIGS_GUILDS_DIRECTORY):
+        os.makedirs(CONFIGS_GUILDS_DIRECTORY)
 
     # create 'logs' directory
     if not os.path.isdir(LOGS_DIRECTORY):
-        os.mkdir(LOGS_DIRECTORY)
+        os.makedirs(LOGS_DIRECTORY)
 
     # setup logging
     logging.basicConfig(
