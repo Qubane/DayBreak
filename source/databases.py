a189d7dd6a40eac57957189f646994bcfd80dd54
UltraQbik <no1skill@yandex.ru>
2025-07-14 18:15:17 +0300

    Add connect_database

--- source/databases.py
+++ source/databases.py
@@ -4,3 +4,15 @@ Handling of databases
 
 
 import aiosqlite
+from source.settings import *
+
+
+async def connect_database(module_name: str) -> aiosqlite.Connection:
+    """
+    Connects and returns database
+    :param module_name: name of the module
+    :return: database connection
+    """
+
+    database_path = f"{VARS_DIRECTORY}/{module_name}.sqlite"
+    return await aiosqlite.connect(database_path)
