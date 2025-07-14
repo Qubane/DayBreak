3aa5ae11e8278c4760b9f2fc3395713ac8a8a5e8
UltraQbik <no1skill@yandex.ru>
2025-07-15 00:15:47 +0300

    Delete decorator and call

--- modules/Sentiments/main.py
+++ modules/Sentiments/main.py
@@ -72,7 +72,6 @@ class SentimentsModule(commands.Cog):
         # database
         self.db_handle: DatabaseHandle = DatabaseHandle(self.module_name)
         self.db: aiosqlite.Connection | None = None
-        asyncio.create_task(self.on_ready())
 
         # configs
         self.module_config: ModuleConfig = ModuleConfig(self.module_name)
@@ -92,7 +91,6 @@ class SentimentsModule(commands.Cog):
         await self.db_handle.close()
         self.logger.info("Database closed")
 
-    @commands.Cog.listener("on_ready")
     async def on_ready(self) -> None:
         """
         Connect database
