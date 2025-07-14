873c980561818357a8fb3d5010cddccd602bcaa1
UltraQbik <no1skill@yandex.ru>
2025-07-14 16:36:13 +0300

    Make DotDict work for lists

--- source/utils.py
+++ source/utils.py
@@ -10,7 +10,12 @@ class DotDict(dict):
 
     def __getattr__(self, item):
         val = super().__getitem__(item)
-        return DotDict(val) if type(val) is dict else val
+        if isinstance(val, dict):
+            return DotDict(val)
+        elif isinstance(val, list):
+            return [DotDict(x) for x in val]
+        else:
+            return val
 
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
