33af5642ae272a2a3adc82c7e3120e0acdf0f834
UltraQbik <no1skill@yandex.ru>
2025-07-14 16:13:13 +0300

    Make DotDict recursively apply

--- source/utils.py
+++ source/utils.py
@@ -8,6 +8,9 @@ class DotDict(dict):
     dot.notation access to dictionary attributes
     """
 
-    __getattr__ = dict.get
+    def __getattr__(self, item):
+        val = super().__getitem__(item)
+        return DotDict(val) if type(val) is dict else val
+
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
