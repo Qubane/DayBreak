be11581f117c4d02c1f05183e0b23ec912bca5cc
UltraQbik <no1skill@yandex.ru>
2025-07-15 13:49:44 +0300

    Carry out utils from core

--- source/utils.py
+++ source/utils.py
@@ -3,6 +3,34 @@ Some random utility classes
 """
 
 
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+async def is_bot_owner(client: commands.Bot, interaction: discord.Interaction) -> bool:
+    """
+    Checks if the interaction was called by bot owner
+    :param client: bot client
+    :param interaction: app commands interaction
+    :return: True if interaction was called by bot owner
+    """
+
+    return await client.is_owner(interaction.user)
+
+
+async def check_bot_ownership(client: commands.Bot, interaction: discord.Interaction) -> None:
+    """
+    Same as 'is_bot_owner' except it raises an error instead
+    :param client: bot client
+    :param interaction: app commands interaction
+    """
+
+    if not (await is_bot_owner(client, interaction)):
+        raise commands.MissingPermissions(
+            ["bot_owner"], "You must be a host of this bot to run this command")
+
+
 class DotDict(dict):
     """
     dot.notation access to dictionary attributes
