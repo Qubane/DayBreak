51c9b0b17bfccfab1f703c5af75ebadd609930dd
UltraQbik <no1skill@yandex.ru>
2025-07-15 14:05:55 +0300

    Add HostUtils module

---
+++ modules/HostUtils/main.py
@@ -0,0 +1,49 @@
+"""
+This module adds commands that can only by used by owner of the bot (aka bot host)
+"""
+
+
+import discord
+import logging
+from discord import app_commands
+from discord.ext import commands
+from source.utils import *
+from source.configs import *
+from source.databases import *
+
+
+class HostUtilsModule(commands.Cog):
+    """
+    This is the HostUtils module
+    """
+
+    def __init__(self, client: commands.Bot) -> None:
+        self.client: commands.Bot = client
+        self.module_name: str = "HostUtils"
+
+        # logging
+        self.logger: logging.Logger = logging.getLogger(__name__)
+        self.logger.info("Module loaded")
+
+        # configs
+        self.guild_configs: GuildConfigCollection = GuildConfigCollection(self.module_name)  # per guild config
+
+    async def on_cleanup(self):
+        """
+        Gets called when the bot is exiting
+        """
+
+    @commands.command("make-announce")
+    async def command_make_announce(
+        self,
+        ctx: commands.Context,
+        *,
+        message: str
+    ) -> None:
+        """
+        This is an announcement command, that is used to update people on bot news
+        """
+
+
+async def setup(client: commands.Bot) -> None:
+    await client.add_cog(HostUtilsModule(client))
